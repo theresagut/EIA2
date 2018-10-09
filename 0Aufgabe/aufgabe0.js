00b205df7f0af27951809834835c53c2d42d00dd
TheresaGut <TheresaGut@theresas-mbp.eduroam.hs-furtwangen.de>
2018-10-09 15:54:32 +0200

    JS datei mit den neuen Einstellung

---
+++ 0Aufgabe/aufgabe0.js
@@ -0,0 +1,9 @@
+// Begrüßung
+function Funktion() {
+    var name = prompt("Wie heißt du?");
+    if (name != null) {
+        document.getElementById("solution").innerHTML =
+            "Wow " + name + " kann klicken.";
+    }
+}
+//# sourceMappingURL=aufgabe0.js.map
