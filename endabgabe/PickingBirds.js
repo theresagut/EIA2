e1249b8eb2e82b9213799ebde68ae9909fd70c3f
Theresa Gut <TheresaGut@theresas-mbp-2.eduroam.hs-furtwangen.de>
2020-02-08 17:12:19 +0100

    birds

--- endabgabe/PickingBirds.js
+++ endabgabe/PickingBirds.js
@@ -1,11 +1,11 @@
 "use strict";
 var Endabgabe;
 (function (Endabgabe) {
-    class Bird extends Endabgabe.Moveable {
+    class PickingBird extends Endabgabe.Moveable {
         constructor(_size, _position) {
             super(_position); // ruft die Position aus der Moveable auf - wieso?
             this.size = 2;
-            console.log("Bird constructor");
+            console.log("picking Bird constructor");
             this.bodycolor = getRandomColor();
             this.billcolor = getRandomColor();
             if (_position)
@@ -13,7 +13,7 @@ var Endabgabe;
             else
                 this.position = new Endabgabe.Vector(Math.random() * crc2.canvas.width, Math.floor(Math.random() * 400));
             // Math Random für Verteilung Breite * Höhe Canvas, Wert (größten & aufgerundet) * random Nr *600 (Verteilung auf Höhe des Canvas)
-            this.velocity = new Endabgabe.Vector(Math.random() - 1 * 4, (Math.random() * 2 + Math.random() - 2));
+            this.velocity = new Endabgabe.Vector(Math.random() - 1 * 7, (Math.random() * 2 + Math.random() - 2));
             // -1 = vorwärts, 7 = Geschw. -2 und +2 (gehen langsam hoch und runter)
         }
         draw() {
@@ -21,8 +21,8 @@ var Endabgabe;
             crc2.save();
             crc2.translate(this.position.x, this.position.y);
             crc2.beginPath();
-            crc2.arc(-25, 20, 15, 0, 2 * Math.PI);
-            crc2.fillStyle = "#af8a54";
+            crc2.arc(-25, 20, 15, 0, 4 * Math.PI);
+            crc2.fillStyle = "blue";
             crc2.fill();
             crc2.restore();
             crc2.closePath();
@@ -36,16 +36,6 @@ var Endabgabe;
             crc2.restore();
             crc2.closePath();
             //bill//Schnabel
-            crc2.save();
-            crc2.translate(this.position.x, this.position.y);
-            crc2.beginPath();
-            crc2.moveTo(-36, 10);
-            crc2.lineTo(-50, 1);
-            crc2.lineTo(-40, 17);
-            crc2.fillStyle = this.billcolor;
-            crc2.fill();
-            crc2.restore();
-            crc2.closePath();
             //body
             crc2.save();
             crc2.translate(this.position.x, this.position.y);
@@ -56,16 +46,6 @@ var Endabgabe;
             crc2.restore();
             crc2.closePath();
             //wings
-            crc2.save();
-            crc2.translate(this.position.x, this.position.y);
-            crc2.beginPath();
-            crc2.moveTo(15, 20);
-            crc2.lineTo(-15, 20);
-            crc2.lineTo(-2, 50);
-            crc2.fillStyle = "#af8a54";
-            crc2.fill();
-            crc2.restore();
-            crc2.closePath();
         }
         isHit(_hotspot) {
             let hitsize = this.size * 20; // radius, indem sie getroffen werden
@@ -74,7 +54,7 @@ var Endabgabe;
             //Entfernung vertikal & horizontal */
         }
     }
-    Endabgabe.Bird = Bird;
+    Endabgabe.PickingBird = PickingBird;
     function getRandomColor() {
         var letters = '0123456789ABCDEF'.split('');
         var color = '#';
@@ -84,4 +64,4 @@ var Endabgabe;
         return color;
     }
 })(Endabgabe || (Endabgabe = {}));
-//# sourceMappingURL=birds.js.map
+//# sourceMappingURL=PickingBirds.js.map
--- endabgabe/PickingBirds.ts
+++ endabgabe/PickingBirds.ts
@@ -1,5 +1,5 @@
-namespace Endabgabe{
-    export class Bird extends Moveable {
+namespace Endabgabe {
+    export class PickingBird extends Moveable {
 
         position: Vector;
         velocity: Vector;
@@ -10,8 +10,8 @@ namespace Endabgabe{
         constructor(_size: number, _position?: Vector) { // position muss nicht genutzt werden, ist optional
 
 
-            super(_position); // ruft die Position aus der Moveable auf - wieso?
-            console.log("Bird constructor");
+           super (_position); // ruft die Position aus der Moveable auf - wieso?
+            console.log("picking Bird constructor");
 
             this.bodycolor = getRandomColor();
             this.billcolor = getRandomColor();
@@ -23,20 +23,19 @@ namespace Endabgabe{
                 this.position = new Vector(Math.random() * crc2.canvas.width, Math.floor(Math.random() * 400));
     // Math Random für Verteilung Breite * Höhe Canvas, Wert (größten & aufgerundet) * random Nr *600 (Verteilung auf Höhe des Canvas)
 
-            this.velocity = new Vector(Math.random() -1 * 4,(Math.random() * 2 + Math.random() - 2));
+            this.velocity = new Vector(Math.random() -1 * 7,(Math.random() * 2 + Math.random() - 2));
     // -1 = vorwärts, 7 = Geschw. -2 und +2 (gehen langsam hoch und runter)
 
         }
 
-        
         draw(): void {
 
             //birdhead
             crc2.save();
             crc2.translate(this.position.x, this.position.y);
             crc2.beginPath();
-            crc2.arc(-25, 20, 15, 0, 2 * Math.PI);
-            crc2.fillStyle = "#af8a54";
+            crc2.arc(-25, 20, 15, 0, 4 * Math.PI);
+            crc2.fillStyle = "blue";
             crc2.fill();
             crc2.restore();
             crc2.closePath();
@@ -50,16 +49,7 @@ namespace Endabgabe{
             crc2.restore();
             crc2.closePath();
             //bill//Schnabel
-            crc2.save();
-            crc2.translate(this.position.x, this.position.y);
-            crc2.beginPath();
-            crc2.moveTo(-36, 10);
-            crc2.lineTo(-50, 1);
-            crc2.lineTo(-40, 17);
-            crc2.fillStyle = this.billcolor;
-            crc2.fill();
-            crc2.restore();
-            crc2.closePath();
+            
             //body
             crc2.save();
             crc2.translate(this.position.x, this.position.y);
@@ -71,16 +61,7 @@ namespace Endabgabe{
             crc2.closePath();
 
             //wings
-            crc2.save();
-            crc2.translate(this.position.x, this.position.y);
-            crc2.beginPath();
-            crc2.moveTo(15, 20);
-            crc2.lineTo(-15, 20);
-            crc2.lineTo(-2, 50);
-            crc2.fillStyle = "#af8a54";
-            crc2.fill();
-            crc2.restore();
-            crc2.closePath();
+            
 
         }
 
